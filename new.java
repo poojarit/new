9232a745a8ababedb5172e68ed8fa36e74fbc7d0
“Akshiv20” <queenprince20@gmail.com>
2023-01-31 21:47:43 +0000

    5thcommit

--- new.java
+++ new.java
@@ -2,7 +2,7 @@ public class VowelConsonant {
 
     public static void main(String[] args) {
 
-        char ch = '1';
+        char ch = '12';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
